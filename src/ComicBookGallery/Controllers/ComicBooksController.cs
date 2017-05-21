44193a6138ed55abab4f6c5ddac2d898e752b3a5
Lily Amber <devilfish13@gmail.com>
2017-05-21 17:32:01 -0400

    Added Detail method to ComicBooksController

--- src/ComicBookGallery/Controllers/ComicBooksController.cs
+++ src/ComicBookGallery/Controllers/ComicBooksController.cs
@@ -6,8 +6,11 @@ using System.Web.Mvc;
 
 namespace ComicBookGallery.Controllers
 {
-    public class ComicBooksController : Controller
+    class ComicBooksController : Controller
     {
-
+        public string Detail()
+        {
+            return "Hello from the comic books controller";
+        }
     }
 }
